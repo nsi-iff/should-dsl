0c74df56bb9b1965e6beea04e7135b4e0b20a95f
rmanhaes <rmanhaes@89f1475a-30cd-11de-a996-7ff70a941223>
2009-05-18 06:11:10 +0000

    Added should_be with no function for identity. rvalue is now mandatory

--- src/should_dsl.py
+++ src/should_dsl.py
@@ -6,6 +6,7 @@ class Should(object):
         self._negate = negate
         self._is_thrown_by = False
         self.should_functions_by_name = dict()
+        self.__is()
     
     def _evaluate(self, value):
         if self._negate:
@@ -18,12 +19,11 @@ class Should(object):
         return ''
     
     def __rlshift__(self, lvalue):
-        return self.__ror__(lvalue)
+        result = self.__ror__(lvalue)
+        return result
     
     def __ror__(self, lvalue):
         self._lvalue = lvalue
-        if not self._has_rvalue:
-            return self._check_assertion()
         return self
     
     def __rshift__(self, rvalue):
@@ -33,75 +33,53 @@ class Should(object):
         self._rvalue = rvalue
         return self._check_assertion()
     
+    def __is(self):
+        '''The default behavior for a should object, called on constructor'''
+        self._func = lambda x, y: x is y
+        self._error_message = '%s is %s%s'
+    
+    def _make_a_copy(self, func, error_message):
+        clone = Should(self._negate)
+        clone._is_thrown_by = self._is_thrown_by
+        clone.should_functions_by_name = self.should_functions_by_name
+        clone._func = func
+        clone._error_message = error_message
+        return clone
+    
     @property
     def equal_to(self):
-        self._func = lambda x, y: x == y
-        self._error_message = lambda x, y: '%s is %sequal to %s' % (x, self._negate_str(), y)
-        self._has_rvalue = True
-        return self
-    
-    @property
-    def true(self):
-        self._func = lambda x: x is True
-        self._error_message = lambda x: '%s is %sTrue' % (x, self._negate_str())
-        self._has_rvalue = False
-        return self
-    
-    @property
-    def false(self):
-        self._func = lambda x: x is False
-        self._error_message = lambda x: '%s is %sFalse' % (x, self._negate_str())
-        self._has_rvalue = False
-        return self
-    
-    @property
-    def none(self):
-        self._func = lambda x: x == None
-        self._error_message = lambda x: '%s is %sNone' % (x, self._negate_str())
-        self._has_rvalue = False
-        return self
-    
+        return self._make_a_copy(func=lambda x, y: x == y, 
+                                error_message='%s is %sequal to %s')
+        
     @property
     def into(self):
-        self._func = lambda item, container: item in container
-        self._error_message = lambda item, container: '%s is %sinto %s' % (item, self._negate_str(), container)
-        self._has_rvalue = True
-        return self 
+        return self._make_a_copy(func=lambda item, container: item in container, 
+                                error_message='%s is %sinto %s')
     
     @property
-    def have(self):
-        self._func = lambda container, item: item in container
-        self._error_message = lambda container, item: '%s does %shave %s' % (container, self._negate_str(), item)
-        self._has_rvalue = True
-        return self
+    def have(self): 
+        return self._make_a_copy(func=lambda container, item: item in container, 
+                                error_message='%s does %shave %s')
     
     @property
     def greater_than(self):
-        self._func = lambda x, y: x > y
-        self._error_message = lambda x, y: '%s is %sgreater than %s' % (x, self._negate_str(), y)
-        self._has_rvalue = True
-        return self 
+        return self._make_a_copy(func=lambda x, y: x > y, 
+                                error_message='%s is %sgreater than %s') 
     
     @property
     def greater_than_or_equal_to(self):
-        self._func = lambda x, y: x >= y
-        self._error_message = lambda x, y: '%s is %sgreater than or equal to %s' % (x, self._negate_str(), y)
-        self._has_rvalue = True
-        return self
+        return self._make_a_copy(func=lambda x, y: x >= y, 
+                                error_message='%s is %sgreater than or equal to %s')
     
     @property
     def less_than(self):
-        self._func = lambda x, y: x < y
-        self._error_message = lambda x, y: '%s is %sless than %s' % (x, self._negate_str(), y)
-        self._has_rvalue = True
-        return self
+        return self._make_a_copy(func=lambda x, y: x < y, 
+                                error_message='%s is %sless than %s')
     
     @property
     def less_than_or_equal_to(self):
-        self._func = lambda x, y: x <= y
-        self._error_message = lambda x, y: '%s is %sless than or equal to %s' % (x, self._negate_str(), y)
-        self._has_rvalue = True
-        return self
+        return self._make_a_copy(func=lambda x, y: x <= y, 
+                                error_message='%s is %sless than or equal to %s')
     
     @property
     def thrown_by(self):
@@ -111,39 +89,31 @@ class Should(object):
                 return False
             except exception:
                 return True
-        self._func = check_exception
-        self._error_message = lambda exception, callable: '%s is %sthrown by %s' % (exception, self._negate_str(), callable)
-        self._has_rvalue = True
-        self._is_thrown_by = True
-        return self
+        clone = self._make_a_copy(func=check_exception, 
+                                 error_message='%s is %sthrown by %s')
+        clone._is_thrown_by = True
+        return clone
     
     def _check_assertion(self):
-        if self._has_rvalue:
-            evaluation = None 
-            if self._is_thrown_by and self._rvalue.__class__ in (tuple, list, dict) and len(self._rvalue) > 1:
-                evaluation = self._evaluate(self._func(self._lvalue, self._rvalue[0], *self._rvalue[1:]))
-            else:
-                evaluation = self._evaluate(self._func(self._lvalue, self._rvalue)) 
-            if not evaluation:
-                raise ShouldNotSatisfied, self._error_message(self._lvalue, self._rvalue)
-            else:
-                return True
+        evaluation = None 
+        if self._is_thrown_by and self._rvalue.__class__ in (tuple, list, dict) and len(self._rvalue) > 1:
+            evaluation = self._evaluate(self._func(self._lvalue, self._rvalue[0], *self._rvalue[1:]))
         else:
-            if not self._evaluate(self._func(self._lvalue)):
-                raise ShouldNotSatisfied, self._error_message(self._lvalue)
-            else:
-                return True
+            evaluation = self._evaluate(self._func(self._lvalue, self._rvalue)) 
+        if not evaluation:
+            raise ShouldNotSatisfied, self._error_message % (self._lvalue, self._negate_str(), self._rvalue)
+        else:
+            return True
             
     def add_should(self, function):
         '''Adds a new should case.
         The function must return a tuple (or any other __getitem__ compatible object)
-        containing three elements:
+        containing two elements:
         [0] = a function taking one or two parameters, that will do the desired comparison
         [1] = the error message. this message must contain three %s placeholders. By example,
         "%s is %snicer than %s" can result in "Python is nicer than Ruby" or 
-        "Python is not nicer than Ruby" depending whether <<should_be.function_name>> or
-        <<should_not_be.function_name>> be applied.
-        [2] True if there is a rvalue, otherwise False
+        "Python is not nicer than Ruby" depending whether |should_be.function_name| or
+        |should_not_be.function_name| be applied.
         '''
         self.should_functions_by_name[function.__name__] = function
 
@@ -152,17 +122,14 @@ class Should(object):
             try:
                 function = self.should_functions_by_name[str(method_info)]
                 result = function()
-                self._func = result[0]
-                error_message = result[1]
-                self._has_rvalue = result[2]
-                if self._has_rvalue:
-                    self._error_message = lambda x, y: error_message % (x, self._negate_str(), y)
-                else:
-                    self._error_message = lambda x: error_message % (x, self._negate_str()) 
-                return self
+                clone = self._make_a_copy(func=result[0], error_message=result[1])
+                return clone
             except KeyError:
                 raise AttributeError, "'%s' object has no attribute '%s'" % (self.__class__, str(method_info))
         return method_missing()
+    
+    def _message_to_lambda(self, error_message):
+        return lambda x, y: error_message % (x, self._negate_str(), y)
 
             
 class ShouldNotSatisfied(Exception):
@@ -176,4 +143,4 @@ should_not_have = Should(negate=True).have
 def should_case(method):
     should_be.add_should(method)
     should_not_be.add_should(method)
-    return method   
+    return method
