f17a7f00e2e1f1393ad27ec459bf7ceab41d73a0
Hugo Lopes Tavares <hltbra@gmail.com>
2009-06-17 17:04:42 -0300

    added 'dev' to version; `long_description` is the README.rst; improved the `description`; added `classifiers`; updated `url`

--- setup.py
+++ setup.py
@@ -1,17 +1,29 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.3'
+version = '0.3dev'
+
+readme_fd = open('README.rst')
+readme = readme_fd.read()
+readme_fd.close()
 
 setup(name='should_dsl',
       version=version,
-      description="Should DSL",
-      long_description="",
-      classifiers=[],
-      keywords='bdd dsl python',
+      description='Should assertions in Python as clear and readable as possible'
+      long_description=readme,
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python',
+          'Topic :: Software Development :: Documentation',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Testing',
+      ],
+      keywords='should dsl assertion bdd python',
       author='Hugo Lopes Tavares',
       author_email='hltbra@gmail.com',
-      url='http://code.google.com/p/should-dsl/',
+      url='http://github.com/hugobr/should-dsl',
       license='MIT License',
       packages=['should_dsl'],
       package_dir={'should_dsl': 'src'},
