f7ceed09042408f72cf60d33fe1f90c03a18fdfe
Hugo Lopes Tavares <hltbra@gmail.com>
2009-06-19 14:14:57 -0300

    added unittest example of use

--- src/run_all_doctests.py
+++ src/run_all_doctests.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 import doctest
 import os
+from tests import unittest_example
+
+flags = doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS
 
 if __name__ == '__main__':
     doctests_path = os.path.join(os.path.dirname(__file__), 'doctests')
@@ -8,4 +11,7 @@ if __name__ == '__main__':
         if not doctest_file.endswith('.txt'):
             continue
         doctest.testfile(os.path.join(doctests_path, doctest_file),
-                         optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS)
+                         optionflags=flags)
+
+    for module in [unittest_example,]:
+        doctest.testmod(module, optionflags=flags)
---
+++ src/tests/unittest_example.py
@@ -0,0 +1,44 @@
+'''
+    >>> output = StringIO()
+    >>> runner = unittest.TextTestRunner(stream=output)
+    >>> loader = unittest.TestLoader()
+    >>> suite = loader.loadTestsFromTestCase(UsingShouldExample)
+    >>> runner.run(suite)
+    <unittest._TextTestResult run=8 errors=0 failures=4>
+
+'''
+
+import unittest
+from cStringIO import StringIO
+from should_dsl import (should_be, should_not_be,
+                        should_have, should_not_have)
+
+
+class UsingShouldExample(unittest.TestCase):
+    def test_showing_should_be_fail(self):
+        'hello world!' |should_be| 'Hello World!'
+
+    def test_showing_should_be_work(self):
+        1+1 |should_be| 2
+
+    def test_showing_should_not_be_fail(self):
+        'bdd' |should_not_be| 'bdd'
+        # equal_to doesn't compare ids, but content
+        'hi' |should_not_be.equal_to| 'HI'.lower()
+
+    def test_showing_should_not_be_work(self):
+        'hello world!' |should_not_be| 'Hello World!'
+        # the objects have different ids below
+        'hi' |should_not_be| 'HI'.lower()
+
+    def test_showing_should_have_fail(self):
+        [1, 2, 3] |should_have| 5
+
+    def test_showing_should_have_work(self):
+        'hello world!' |should_have| 'world'
+
+    def test_showing_should_not_have_fail(self):
+        {'one': 1, 'two': 2} |should_not_have| 'two'
+
+    def test_showing_should_not_have_work(self):
+        ["that's", 'all', 'folks'] |should_not_have| "that"
