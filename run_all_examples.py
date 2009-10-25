2ea2bbde6ea400b39f7bec641a975a41af88d720
Hugo Lopes Tavares <hltbra@gmail.com>
2009-10-25 17:26:04 -0200

    updated examples and removed not more used unittest examples at src/tests

---
+++ run_all_examples.py
@@ -0,0 +1,17 @@
+import doctest
+import unittest
+import os
+
+flags = doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS
+
+if __name__ == '__main__':
+    doctests_path = os.path.join(os.path.dirname(__file__),  'src', 'doctests')
+    suite = unittest.TestSuite()
+    runner = unittest.TextTestRunner()
+
+    suite.addTest(doctest.DocFileSuite('README.rst'))
+    for doctest_file in os.listdir(doctests_path):
+        if doctest_file.endswith('.txt'):
+            suite.addTest(doctest.DocFileSuite(os.path.join(doctests_path, doctest_file),
+                                               optionflags=flags))
+    runner.run(suite)
