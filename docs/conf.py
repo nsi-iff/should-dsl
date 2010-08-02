6e1a2f9509e51f181414cd828ad88c3bc8c2658e
Hugo Lopes Tavares <hltbra@gmail.com>
2010-08-02 14:17:46 -0300

    removed copyright line and changed code syntax highlight

--- docs/conf.py
+++ docs/conf.py
@@ -81,7 +81,8 @@ exclude_patterns = ['_build']
 #show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'default'
+#pygments_style = 'default'
+pygments_style = 'vs'
 #['manni', 'perldoc', 'borland', 'colorful', 'default', 'murphy', 'trac', 'fruity', 'autumn', 'bw', 'emacs', 'pastie', 'friendly', 'native']
 
 
@@ -157,7 +158,7 @@ html_static_path = ['_static']
 #html_show_sphinx = True
 
 # If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
-#html_show_copyright = True
+html_show_copyright = False
 
 # If true, an OpenSearch description file will be output, and all pages will
 # contain a <link> tag referring to it.  The value of this option must be the
