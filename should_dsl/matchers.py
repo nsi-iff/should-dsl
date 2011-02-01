3d71c54046e2bf3b7aa82d06f8a6041a6540361a
Rodrigo Manhães <rmanhaes@gmail.com>
2011-02-01 00:21:57 -0200

    added matcher 'be_empty'

--- should_dsl/matchers.py
+++ should_dsl/matchers.py
@@ -604,3 +604,24 @@ class IncludeValues(IncludeDictElement):
 
 matcher(IncludeValues)
 
+
+class BeEmpty(object):
+
+    name = 'be_empty'
+
+    def __call__(self):
+        return self
+
+    def match(self, container):
+        self._container = container
+        return len(container) == 0
+
+    def message_for_failed_should(self):
+        return "expected %s to be empty" % repr(self._container)
+
+    def message_for_failed_should_not(self):
+        return "expected %s not to be empty" % repr(self._container)
+
+
+matcher(BeEmpty)
+
