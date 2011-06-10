b15506db5f0b597a16c0f57b7b449e95faf9ac4e
Rodrigo Manhães <rmanhaes@gmail.com>
2011-06-10 15:22:24 -0300

    allow ShouldNotSatisfied to be imported from should_dsl

--- should_dsl/__init__.py
+++ should_dsl/__init__.py
@@ -1,6 +1,7 @@
 from should_dsl.dsl import (should,
                          should_not,
                          matcher,
-                         add_predicate_regex,)
+                         add_predicate_regex,
+                         ShouldNotSatisfied)
 from should_dsl import matchers
 
