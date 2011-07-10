dcdd315ff24bd0e27c77100cc2ff1e6d24f688aa
Hugo Lopes Tavares <hltbra@gmail.com>
2011-07-10 11:47:53 -0300

    extracted method _find_matcher

--- should_dsl/subject.py
+++ should_dsl/subject.py
@@ -8,17 +8,17 @@ class Subject(object):
         self._obj = obj
 
     def should(self, **kwargs):
-        matcher_name = kwargs.keys()[0]
-        matcher_args = kwargs[matcher_name]
-        matcher = self._api.find_matcher(matcher_name)
+        matcher, matcher_args = self._find_matcher(kwargs)
         if not matcher.match(matcher_args):
             raise ShouldNotSatisfied(matcher.message_for_failed_should())
 
     def should_not(self, **kwargs):
-        matcher_name = kwargs.keys()[0]
-        matcher_args = kwargs[matcher_name]
-        matcher = self._api.find_matcher(matcher_name)
+        matcher, matcher_args = self._find_matcher(kwargs)
         if matcher.match(matcher_args):
             raise ShouldNotSatisfied(matcher.message_for_failed_should_not())
 
-
+    def _find_matcher(self, kwargs):
+        matcher_name = kwargs.keys()[0]
+        matcher_args = kwargs[matcher_name]
+        matcher = self._api.find_matcher(matcher_name)
+        return matcher, matcher_args
